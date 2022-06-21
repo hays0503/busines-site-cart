1303a8aeb5c2953f2d4b69fd0eb8ea7b2e33033e
hays hays0503 <cthtufgbv@mail.ru>
2022-06-21 17:31:49 +0600

    fix

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ particlesJS("particles-js", {
         nb_sides: 5,
       },
       image: {
-        src: "../img/sakyra.png",
+        src: "./img/sakyra.png",
         width: 100,
         height: 100,
       },
